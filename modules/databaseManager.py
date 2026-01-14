4aad7678068ec1198be2d996efc07abbca72edeb
Bogdane Braoun <112286@ecole-it.com>
2026-01-14 16:19:10 +0100

    add the function db_return that returns the database. We will use this a lot later

--- modules/databaseManager.py
+++ modules/databaseManager.py
@@ -1,13 +1,30 @@
-def db_print(category):
-    """
-    Docstring for db_print
-    
-    :param param:
-    "all" - print all the database.
-    
-    """
-    pass
+import json
+import os 
+
 
 def db_read():
-    pass
+    """
+    Docstring for db_print
+    returns the database in the form of 
+    {
+        "dataindex" : 
+        {
+            "site": "data",
+            "categorie" : "data",
+            "email" : "data",
+            "mdp" : "data", 
+            "date_creation" : "data",
+            "score" : "data"
+        }
+    }
+    """
+    base_dir = os.path.dirname(os.path.abspath(__file__)) #takes the current directory name
+    project_dir = os.path.dirname(base_dir) #takes the previous directory name
+    db_path = os.path.join(project_dir, "database", "DB.json") #finds the path to the .json database
+    with open(db_path, "r") as fichier:
+        data = json.load(fichier)
+        return data
+
+
+
 
