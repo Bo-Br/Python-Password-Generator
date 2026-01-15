dbb979c46aed0393487e0f5fc7eebee838be40dc
4h4ze <4h4ze@proton.me>
2026-01-15 10:41:34 +0100

    Initialised analyse.py and generate.py

---
+++ modules/generate.py
@@ -0,0 +1,33 @@
+import secrets
+
+# Définition des tuples de caractères sécurisés
+letters = ("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "m", "n",
+           "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y","z","A","B",
+           "C", "D", "E", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q",
+           "R", "S", "T", "U", "V", "W", "X", "Y", "Z")
+
+numbers = ("2", "3", "4", "5", "6", "7", "8", "9")
+
+special_character = ("!", "#", "$", "%", "&", "*", "+", "-", "=", "?", "@", "^", "_")
+
+score_password = {
+    "0" : "Porte ouverte",
+    "1" : "Très mauvaise",
+    "2" : "Mauvais",
+    "3" : "Moyen",
+    "4" : "Convenable",
+    "5" : "Bon",
+    "6" : "Très bon",
+}
+
+
+
+def generate (password_length):
+    
+    password = ""
+    pool = letters + numbers + special_character
+    
+    for i in range(0 , password_length): 
+        password += str(secrets.choice(pool))
+    print(password)
+    return password
