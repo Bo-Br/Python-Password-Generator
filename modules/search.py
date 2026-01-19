20437a44f0611b7dae9aa32a42fac5902f317ab1
Bogdane Braoun <112286@ecole-it.com>
2026-01-19 15:31:19 +0100

    add a search function and extract the print database function so it can be used

--- modules/search.py
+++ modules/search.py
@@ -1,4 +1,20 @@
 from database_manager import db_read
+
+
+def db_print(db):
+    """
+    Docstring for db_print
+    
+    :param db: Description
+    """
+    if db:
+        for db_id in db.keys():
+                print(f"{db_id} : ")
+                for db_key, db_value in db[db_id].items():
+                    print(f"{db_key} : {db_value}")
+    else:
+        print("Error, database Empty")
+
 def search(category, name):
     """
     Description:
@@ -10,16 +26,12 @@ def search(category, name):
     """
     db_copy = db_read()
     to_return = []
-    for db_keys, db_value in db_copy.items():
+    for db_keys in db_copy.keys():
         for i in db_keys:
             if db_copy[i][category] == name:
                 to_return += [i]
-        if not to_return:
-            print("Error, please retry.")
-            return None
-        else:
-            return to_return
-
-            
-        
-
+    if not to_return:
+        print("Error, please retry.")
+        return None
+    else:
+        db_print({str(to_return[0]) : (db_copy[str(to_return[0])])})
